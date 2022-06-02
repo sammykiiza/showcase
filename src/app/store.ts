32d0ad95c600e546d6b3ff48792ffb6ab51d44c7
sammykiiza <sammykiiza@gmail.com>
2022-06-02 20:55:37 +0300

    added side nav submenus

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import dropDownReducer from '../features/menus/dropDownSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    dropDown: dropDownReducer,
   },
 });
 
--- src/features/menus/SideMenu.tsx
+++ src/features/menus/SideMenu.tsx
@@ -1,25 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { MapIcon, ChartPieIcon, PresentationChartLineIcon, CurrencyDollarIcon, ClipboardListIcon, UserIcon } from '@heroicons/react/solid'
-
-
-interface Navigation {
-  name: string,
-  href: string,
-  icon: React.FC,
-  current: boolean
-}
-
-const navigation: Navigation[] = [
-  { name: 'Map', href: '', icon:MapIcon, current: true },
-  { name: 'Dashboard', href: 'dashboard', icon:ChartPieIcon, current: false },
-  { name: 'Road Network', href: 'road-network', icon:ChartPieIcon, current: false },
-  { name: 'Road Inventory', href: 'road-inventory', icon:ChartPieIcon, current: false },
-  { name: 'Planning', href: 'planning', icon:PresentationChartLineIcon, current: false },
-  { name: 'Finance', href: 'finance', icon:CurrencyDollarIcon, current: false },
-  { name: 'Contract Management', href: 'contract-management', icon:ClipboardListIcon, current: false },
-  { name: 'User Management', href: 'user-management', icon:UserIcon, current: false },
-]
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import {RootState} from '../../app/store'
+import { toggle1, toggle2, toggle3, toggle4, toggle5, toggle6, toggle7 } from './dropDownSlice'
 
 type IconProps = {
   name:React.FC
@@ -34,28 +18,204 @@ function classNames(...classes:string[]) {
 }
 
 function SideMenu() {
+  const dropDown1 = useAppSelector((state:RootState) => state.dropDown.open1);
+  const dropDown2 = useAppSelector((state:RootState) => state.dropDown.open2);
+  const dropDown3 = useAppSelector((state:RootState) => state.dropDown.open3);
+  const dropDown4 = useAppSelector((state:RootState) => state.dropDown.open4);
+  const dropDown5 = useAppSelector((state:RootState) => state.dropDown.open5);
+  const dropDown6 = useAppSelector((state:RootState) => state.dropDown.open6);
+  const dropDown7 = useAppSelector((state:RootState) => state.dropDown.open7);
+  const dispatch = useAppDispatch();
   return (
-    <div className="flex absolute top-0 min-h-screen w-[30%] md:w-[15%] bg-gray-900">
-    <div>
-      <div className="mb-10 pt-5">
-        {navigation.map((item) => (
-          <div className='flex flex-row hover:bg-sky-900'> 
-            <div className='text-white basis-[12.5%] ml-2 mt-3'>
-              <Icon name={item.icon}/>
+    <div className="flex flex-col absolute top-0 left-0 min-h-screen w-[30%] md:w-[15%] bg-gray-900">
+      <div>
+        <div className="mb-10">
+          <div className='px-6 py-4 flex hover:bg-sky-900'> 
+            <div className='basis-[22%] text-white'>
+              <Icon name={MapIcon}/>
             </div>
-            <Link 
-              to={`/${item.href}`}
-              key={item.name}
+            <Link
+              to=''
               className={classNames(
-                "flex items-center text-[17px] px-6 py-2.5 text-white hover:text-white basis-[87.5%]"
+                "basis-[78%] ml-[10px] mt-1 text-base text-white hover:text-white"
               )}
             >
-              {item.name}
+              Dashboard
             </Link>
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+
+      <div className='-mt-[38px]'>
+        <div className="mb-10">
+          <div className='px-6 py-4 flex hover:bg-sky-900'> 
+            <div className='basis-[22%] text-white'>
+              <Icon name={ChartPieIcon}/>
+            </div>
+            <Link
+              to=''
+              className={classNames(
+                "basis-[78%] ml-[10px] mt-1 text-base text-white hover:text-white"
+              )}
+              onClick={() => dispatch(toggle1())}
+            >
+              Road Network
+            </Link>
+          </div>
+
+          <div className={(!dropDown1? 'hidden': 'block') + ' flex flex-col mx-8'}>
+            <a href="/" className='text-white mt-3'>Roads</a>
+            <a href="/" className='text-white mt-3'>Regions</a>
+            <a href="/" className='text-white mt-3'>Locations</a>
+            <a href="/" className='text-white mt-3'>Road Types</a>
+          </div>
+        </div>
+      </div>
+
+      <div className='-mt-[38px]'>
+        <div className="mb-10">
+          <div className='px-6 py-4 flex hover:bg-sky-900'> 
+            <div className='basis-[22%] text-white'>
+              <Icon name={ChartPieIcon}/>
+            </div>
+            <Link
+              to=''
+              className={classNames(
+                "basis-[78%] ml-[10px] mt-1 text-base text-white hover:text-white"
+              )}
+              onClick={() => dispatch(toggle2())}
+            >
+              Road Inventory
+            </Link>
+          </div>
+
+          <div className={(!dropDown2? 'hidden': 'block') + ' flex flex-col mx-8'}>
+            <a href="/" className='text-white mt-3'>Inventory Data</a>
+            <a href="/" className='text-white mt-3'>Road and Section Deta</a>
+          </div>
+        </div>
+      </div>
+
+      <div className='-mt-[38px]'>
+        <div className="mb-10">
+          <div className='px-6 py-4 flex hover:bg-sky-900'> 
+            <div className='basis-[22%] text-white'>
+              <Icon name={PresentationChartLineIcon}/>
+            </div>
+            <Link
+              to=''
+              className={classNames(
+                "basis-[78%] ml-[10px] mt-1 text-base text-white hover:text-white"
+              )}
+              onClick={() => dispatch(toggle7())}
+            >
+              Road Condition
+            </Link>
+          </div>
+
+          <div className={(!dropDown7? 'hidden': 'block') + ' flex flex-col mx-8'}>
+            <a href="/" className='text-white mt-3'>Functional Assesment</a>
+            <a href="/" className='text-white mt-3'>Material Information</a>
+            <a href="/" className='text-white mt-3'>Surface Distress</a>
+          </div>
+        </div>
+      </div>
+
+      <div className='-mt-[38px]'>
+        <div className="mb-10">
+          <div className='px-6 py-4 flex hover:bg-sky-900'> 
+            <div className='basis-[22%] text-white'>
+              <Icon name={PresentationChartLineIcon}/>
+            </div>
+            <Link
+              to=''
+              className={classNames(
+                "basis-[78%] ml-[10px] mt-1 text-base text-white hover:text-white"
+              )}
+              onClick={() => dispatch(toggle3())}
+            >
+              Planning
+            </Link>
+          </div>
+
+          <div className={(!dropDown3? 'hidden': 'block') + ' flex flex-col mx-8'}>
+            <a href="/" className='text-white mt-3'>Link 1</a>
+            <a href="/" className='text-white mt-3'>Link 2</a>
+            <a href="/" className='text-white mt-3'>Link 3</a>
+          </div>
+        </div>
+      </div>
+
+      <div className='-mt-[38px]'>
+        <div className="mb-10">
+          <div className='px-6 py-4 flex hover:bg-sky-900'> 
+            <div className='basis-[22%] text-white'>
+              <Icon name={CurrencyDollarIcon}/>
+            </div>
+            <Link
+              to=''
+              className={classNames(
+                "basis-[78%] ml-[10px] mt-1 text-base text-white hover:text-white"
+              )}
+              onClick={() => dispatch(toggle4())}
+            >
+              Finance
+            </Link>
+          </div>
+
+          <div className={(!dropDown4? 'hidden': 'block') + ' flex flex-col mx-8'}>
+            <a href="/" className='text-white mt-3'>Projects/Programs</a>
+          </div>
+        </div>
+      </div>
+
+      <div className='-mt-[38px]'>
+        <div className="mb-10">
+          <div className='px-6 py-4 flex hover:bg-sky-900'> 
+            <div className='basis-[22%] mt-1 text-white'>
+              <Icon name={ClipboardListIcon}/>
+            </div>
+            <Link
+              to=''
+              className={classNames(
+                "basis-[78%] ml-[10px] text-base text-white hover:text-white"
+              )}
+              onClick={() => dispatch(toggle5())}
+            >
+              Contract Management
+            </Link>
+          </div>
+
+          <div className={(!dropDown5? 'hidden': 'block') + ' flex flex-col mx-8'}>
+            <a href="/" className='text-white mt-3'>Contracts</a>
+            <a href="/" className='text-white mt-3'>Contractors</a>
+          </div>
+        </div>
+      </div>
+
+      <div className='-mt-[38px]'>
+        <div className="mb-10">
+          <div className='px-6 py-4 flex hover:bg-sky-900'> 
+            <div className='basis-[22%] mt-1 text-white'>
+              <Icon name={UserIcon}/>
+            </div>
+            <Link
+              to=''
+              className={classNames(
+                "basis-[78%] ml-[10px] text-base text-white hover:text-white"
+              )}
+              onClick={() => dispatch(toggle6())}
+            >
+              User Management
+            </Link>
+          </div>
+
+          <div className={(!dropDown6? 'hidden': 'block') + ' flex flex-col mx-8'}>
+            <a href="/" className='text-white mt-3'>Users</a>
+            <a href="/" className='text-white mt-3'>Roles</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
