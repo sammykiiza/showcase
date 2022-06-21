bf6ea86a32e6a37b34897921b78d28967245416d
sammykiiza <sammykiiza@gmail.com>
2022-06-21 14:42:48 +0300

    added dynamic data to filterMenu

---
+++ src/features/RoadNetwork/RoadTypes/RoadTypes.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RoadTypes() {
+  return (
+    <div>RoadTypes</div>
+  )
+}
+
+export default RoadTypes
--- src/features/menus/fiterMenu/FilterMenu.tsx
+++ src/features/menus/fiterMenu/FilterMenu.tsx
@@ -3,29 +3,31 @@ import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { checkFilterLocalStorage } from '../../../helpers/functions/checkFilterLocalStorage';
 import { RootState } from '../../../app/store'
 import { setCouncil, setFiscalYear, setRegion } from './filterMenuSlice';
+import { api, useFiscalYearsQuery } from '../../../services/api';
 
 function FilterMenu() {
     const menuState = useAppSelector(((state:RootState) => state.dropDown.closeMenu));
     const dispatch = useAppDispatch();
     const fiscalYear = localStorage.getItem('fiscalYear');
     const region = localStorage.getItem('region');
-    const council = localStorage.getItem('council')
+    const council = localStorage.getItem('council');
+
+    const fiscalYears = useFiscalYearsQuery();
+
+    const [ triggerRegions, regions, promiseRegions ] = api.useLazyRegionsQuery({refetchOnFocus:true});
+
+    const [ triggerLocations, locations, promiseLocations ] = api.useLazyLocationsQuery({refetchOnFocus:true});
 
     useEffect(() => {
-        checkFilterLocalStorage()
-    }, 
-        [
-        fiscalYear,
-        region,
-        council
-    ]
+        checkFilterLocalStorage();
+    },
+        [fiscalYear, region, council]
     );
 
     const changeLocation = () => {
         localStorage.clear();
         window.location.reload();
     }
-
 
   return (
     <>
@@ -46,30 +48,44 @@ function FilterMenu() {
             className="form-multiselect px-8 py-2 mt-2 rounded-full mr-3" 
             name="Fiscal Year"
             id="fiscal-year"
-            onChange={(e) => dispatch(setFiscalYear(e.currentTarget.value))}
+            onChange={(e) => {
+                dispatch(setFiscalYear(e.currentTarget.value))
+            }}
             >
                 <option>Fiscal Year*</option>
-                <option value="2022">2022</option>
+                {fiscalYears?.data?.map((year) => (
+                    <option key={year.id} value={year.id}>{year.name}</option>
+                ))}
             </select>
 
             <select
             className="form-multiselect px-8 py-2 mt-2 rounded-full mr-3" 
             name="Region"
             id="region"
-            onChange={(e) => dispatch(setRegion(e.currentTarget.value))}
+            onClick={async () => { await triggerRegions() }}
+            onChange={(e) => {
+                dispatch(setRegion(e.currentTarget.value))
+            }}
             >
-                <option>Region*</option>
-                <option value="lusaka">Lusaka</option>
+                {regions.currentData === undefined || 
+                regions.isFetching || regions.isLoading || !regions.isSuccess? <option>Loading...</option> : <option value="0">Regions*</option>}
+                {regions.data?.map((region) => (
+                    <option key={region.id} value={region.id}>{region.name}</option>
+                ))}         
             </select>
 
             <select
-            className="form-multiselect px-8 py-2 mt-2 rounded-full mr-3" 
+            className="form-multiselect px-8 py-2 mt-2 rounded-full mr-3"
             name="Council"
             id="council"
+            onClick={async () => {await triggerLocations()}}
             onChange={(e) => dispatch(setCouncil(e.currentTarget.value))}
             >
-                <option>Council*</option>
-                <option value="Chongwe">Chongwe</option>
+                {locations.currentData === undefined || locations.isFetching
+                 || locations.isLoading || !locations.isSuccess? <option>Loading</option> : <option value="0">Council*</option>}
+                {locations.data?.map((location) => (
+                    <option key={location.id} value={location.id}>{location.name}</option>
+                ))}
             </select>
         </div>
     </>
