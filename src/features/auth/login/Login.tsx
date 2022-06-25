bb664943b5e61f54a7660e1ed522d2558e013eae
sammykiiza <sammykiiza@gmail.com>
2022-06-25 18:09:57 +0300

    added error handling on login

--- src/features/auth/login/Login.tsx
+++ src/features/auth/login/Login.tsx
@@ -11,12 +11,13 @@ function Login() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loginError, setLoginError] = useState("");
 
-    const [login, result] = useLoginMutation();
+    const [login, { isLoading, error }] = useLoginMutation();
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
 
-    const loginCredentials = useAppSelector((state:RootState) => state.auth);
+    const loginCredentials = useAppSelector((state: RootState) => state.auth);
 
     const handleLogin = async (e: React.ChangeEvent<any>) => {
         e.preventDefault();
@@ -28,17 +29,23 @@ function Login() {
         try {
             await login(loginRequest).unwrap();
             navigate('/map');
-          } catch (error) {
-            console.error('rejected', error);
-          }
+        } catch (error: any) {
+            setLoginError(error.data.message);
+        }
     }
     return (
         <AuthLayout>
+            <div className={(error ? "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" : 'hidden')} role="alert">
+                <strong className="ml-[38%] font-bold">Error!</strong>
+                <span className="pl-2 block sm:inline">{loginError}</span>
+                <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+                </span>
+            </div>
             <div className='ml-[39%] pt-[8%]'>
                 <div className="w-full max-w-xs">
                     <h1 className='text-center text-xl text-white font-bold pb-2'>ITT-RAMS</h1>
-                    <p className={(result.error? 'absolute top-[24%] left-[41%] text-red-500 text-sm italic' : 'hidden')}>Wrong credentials</p>
-                    <div className={(result.isLoading? 'absolute top-0 left-0 bg-black opacity-50 w-screen h-screen' : 'hidden')}>
+                    <div className={(isLoading ? 'absolute top-0 left-0 bg-black opacity-50 w-screen h-screen' : 'hidden')}>
                         <div className="absolute left-[46%] top-[43%] flex items-center justify-center space-x-2 animate-pulse">
                             <div className="w-8 h-8 bg-blue-400 rounded-full"></div>
                             <div className="w-8 h-8 bg-blue-400 rounded-full"></div>
