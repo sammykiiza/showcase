097c3900ea8c29fe7e72409d7d95bb0a7ad3b230
sammykiiza <sammykiiza@gmail.com>
2022-06-30 14:51:05 +0300

    installed tw-elements

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tw-elements';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Contracts from './features/ContractManagement/Contracts';
