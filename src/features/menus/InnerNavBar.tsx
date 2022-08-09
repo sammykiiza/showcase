d9077db80e7fc50503847e356475908d38885294
sammykiiza <sammykiiza@gmail.com>
2022-08-09 10:07:39 +0300

    added hover effect to menus

--- src/features/menus/InnerNavBar.tsx
+++ src/features/menus/InnerNavBar.tsx
@@ -1,14 +1,24 @@
 import { Popover, Transition } from '@headlessui/react'
 import { ChevronRightIcon } from '@heroicons/react/outline'
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 function InnerNavBar() {
+    let [open1, setOpen1] = useState(false);
+    let [open2, setOpen2] = useState(false);
+    let [open3, setOpen3] = useState(false);
+    let [open4, setOpen4] = useState(false);
+    let [open5, setOpen5] = useState(false);
+    let [open6, setOpen6] = useState(false);
+    let [open7, setOpen7] = useState(false);
     return (
         <>
             <Popover>
                 {({ open }) => (
                     <>
-                        <Popover.Button className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
+                        <Popover.Button
+                        onMouseEnter={() => setOpen1(true)}
+                        onMouseLeave={() => setOpen1(false)}
+                        className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
                             <span>Road Network</span>
                             <ChevronRightIcon
                                 className={`rotate-90 w-3 h-3 ml-2`}
@@ -16,6 +26,7 @@ function InnerNavBar() {
                         </Popover.Button>
 
                         <Transition
+                            show={open1}
                             as={Fragment}
                             enter="transition duration-300 ease-out"
                             enterFrom="transform scale-95 opacity-0"
@@ -26,10 +37,10 @@ function InnerNavBar() {
                         >
                             <Popover.Panel className="absolute z-10 bg-black text-white text-sm w-36 p-2">
                                 <div className="grid grid-cols-1 gap-y-1">
-                                    <a href="/analytics">Analytics</a>
-                                    <a href="/engagement">Engagement</a>
-                                    <a href="/security">Security</a>
-                                    <a href="/integrations">Integrations</a>
+                                    <a href="/analytics">Roads</a>
+                                    <a href="/engagement">Regions</a>
+                                    <a href="/security">Locations</a>
+                                    <a href="/integrations">Road Types</a>
                                 </div></Popover.Panel>
                         </Transition>
                     </>
@@ -39,7 +50,10 @@ function InnerNavBar() {
             <Popover>
                 {({ open }) => (
                     <>
-                        <Popover.Button className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
+                        <Popover.Button
+                        onMouseEnter={() => setOpen2(true)}
+                        onMouseLeave={() => setOpen2(false)} 
+                        className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
                             <span>Road Inventory</span>
                             <ChevronRightIcon
                                 className={`rotate-90 w-3 h-3 ml-2`}
@@ -47,6 +61,7 @@ function InnerNavBar() {
                         </Popover.Button>
 
                         <Transition
+                            show={open2}
                             as={Fragment}
                             enter="transition duration-300 ease-out"
                             enterFrom="transform scale-95 opacity-0"
@@ -57,10 +72,8 @@ function InnerNavBar() {
                         >
                             <Popover.Panel className="absolute z-10 bg-black text-white text-sm w-36 p-2">
                                 <div className="grid grid-cols-1 gap-y-1">
-                                    <a href="/analytics">Analytics</a>
-                                    <a href="/engagement">Engagement</a>
-                                    <a href="/security">Security</a>
-                                    <a href="/integrations">Integrations</a>
+                                    <a href="/analytics">Inventory Data</a>
+                                    <a href="/engagement">Road and Section Data</a>
                                 </div></Popover.Panel>
                         </Transition>
                     </>
@@ -70,7 +83,10 @@ function InnerNavBar() {
             <Popover>
                 {({ open }) => (
                     <>
-                        <Popover.Button className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
+                        <Popover.Button
+                        onMouseEnter={() => setOpen3(true)}
+                        onMouseLeave={() => setOpen3(false)} 
+                        className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
                             <span>Road Condition</span>
                             <ChevronRightIcon
                                 className={`rotate-90 w-3 h-3 ml-2`}
@@ -78,6 +94,7 @@ function InnerNavBar() {
                         </Popover.Button>
 
                         <Transition
+                            show={open3}
                             as={Fragment}
                             enter="transition duration-300 ease-out"
                             enterFrom="transform scale-95 opacity-0"
@@ -88,10 +105,9 @@ function InnerNavBar() {
                         >
                             <Popover.Panel className="absolute z-10 bg-black text-white text-sm w-36 p-2">
                                 <div className="grid grid-cols-1 gap-y-1">
-                                    <a href="/analytics">Analytics</a>
-                                    <a href="/engagement">Engagement</a>
-                                    <a href="/security">Security</a>
-                                    <a href="/integrations">Integrations</a>
+                                    <a href="/analytics">Functional Assesment</a>
+                                    <a href="/engagement">Material Information</a>
+                                    <a href="/security">Surface Distress</a>
                                 </div></Popover.Panel>
                         </Transition>
                     </>
@@ -101,7 +117,10 @@ function InnerNavBar() {
             <Popover>
                 {({ open }) => (
                     <>
-                        <Popover.Button className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
+                        <Popover.Button
+                        onMouseEnter={() => setOpen4(true)}
+                        onMouseLeave={() => setOpen4(false)} 
+                        className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
                             <span>Planning</span>
                             <ChevronRightIcon
                                 className={`rotate-90 w-3 h-3 ml-2`}
@@ -109,6 +128,7 @@ function InnerNavBar() {
                         </Popover.Button>
 
                         <Transition
+                            show={open4}
                             as={Fragment}
                             enter="transition duration-300 ease-out"
                             enterFrom="transform scale-95 opacity-0"
@@ -119,10 +139,7 @@ function InnerNavBar() {
                         >
                             <Popover.Panel className="absolute z-10 bg-black text-white text-sm w-36 p-2">
                                 <div className="grid grid-cols-1 gap-y-1">
-                                    <a href="/analytics">Analytics</a>
-                                    <a href="/engagement">Engagement</a>
-                                    <a href="/security">Security</a>
-                                    <a href="/integrations">Integrations</a>
+                                    <a href="/analytics">Planning</a>
                                 </div></Popover.Panel>
                         </Transition>
                     </>
@@ -132,7 +149,10 @@ function InnerNavBar() {
             <Popover>
                 {({ open }) => (
                     <>
-                        <Popover.Button className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
+                        <Popover.Button
+                        onMouseEnter={() => setOpen5(true)}
+                        onMouseLeave={() => setOpen5(false)} 
+                        className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
                             <span>Finance</span>
                             <ChevronRightIcon
                                 className={`rotate-90 w-3 h-3 ml-2`}
@@ -140,6 +160,7 @@ function InnerNavBar() {
                         </Popover.Button>
 
                         <Transition
+                            show={open5}
                             as={Fragment}
                             enter="transition duration-300 ease-out"
                             enterFrom="transform scale-95 opacity-0"
@@ -150,10 +171,7 @@ function InnerNavBar() {
                         >
                             <Popover.Panel className="absolute z-10 bg-black text-white text-sm w-36 p-2">
                                 <div className="grid grid-cols-1 gap-y-1">
-                                    <a href="/analytics">Analytics</a>
-                                    <a href="/engagement">Engagement</a>
-                                    <a href="/security">Security</a>
-                                    <a href="/integrations">Integrations</a>
+                                    <a href="/analytics">Projects/Programs</a>
                                 </div></Popover.Panel>
                         </Transition>
                     </>
@@ -163,7 +181,10 @@ function InnerNavBar() {
             <Popover>
                 {({ open }) => (
                     <>
-                        <Popover.Button className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
+                        <Popover.Button
+                        onMouseEnter={() => setOpen6(true)}
+                        onMouseLeave={() => setOpen6(false)}
+                         className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
                             <span>Contract Management</span>
                             <ChevronRightIcon
                                 className={`rotate-90 w-3 h-3 ml-2`}
@@ -171,6 +192,7 @@ function InnerNavBar() {
                         </Popover.Button>
 
                         <Transition
+                            show={open6}
                             as={Fragment}
                             enter="transition duration-300 ease-out"
                             enterFrom="transform scale-95 opacity-0"
@@ -181,10 +203,8 @@ function InnerNavBar() {
                         >
                             <Popover.Panel className="absolute z-10 bg-black text-white text-sm w-36 p-2">
                                 <div className="grid grid-cols-1 gap-y-1">
-                                    <a href="/analytics">Analytics</a>
-                                    <a href="/engagement">Engagement</a>
-                                    <a href="/security">Security</a>
-                                    <a href="/integrations">Integrations</a>
+                                    <a href="/analytics">Contracts</a>
+                                    <a href="/engagement">Contractors</a>
                                 </div></Popover.Panel>
                         </Transition>
                     </>
@@ -194,7 +214,10 @@ function InnerNavBar() {
             <Popover>
                 {({ open }) => (
                     <>
-                        <Popover.Button className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
+                        <Popover.Button
+                        onMouseEnter={() => setOpen7(true)}
+                        onMouseLeave={() => setOpen7(false)} 
+                        className={`text-white text-xs lg:text-sm flex items-center text-white hover:bg-[#2271B1] hover:text-white px-3 py-2 lg:py-2.5 font-medium`}>
                             <span>User Management</span>
                             <ChevronRightIcon
                                 className={`rotate-90 w-3 h-3 ml-2`}
@@ -202,6 +225,7 @@ function InnerNavBar() {
                         </Popover.Button>
 
                         <Transition
+                            show={open7}
                             as={Fragment}
                             enter="transition duration-300 ease-out"
                             enterFrom="transform scale-95 opacity-0"
@@ -212,10 +236,8 @@ function InnerNavBar() {
                         >
                             <Popover.Panel className="absolute z-10 bg-black text-white text-sm w-36 p-2">
                                 <div className="grid grid-cols-1 gap-y-1">
-                                    <a href="/analytics">Analytics</a>
-                                    <a href="/engagement">Engagement</a>
-                                    <a href="/security">Security</a>
-                                    <a href="/integrations">Integrations</a>
+                                    <a href="/analytics">Users</a>
+                                    <a href="/engagement">Roles</a>
                                 </div></Popover.Panel>
                         </Transition>
                     </>
